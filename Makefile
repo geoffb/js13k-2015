cc2a185b59c9388be8acd330112bc2e56cf7f211
Geoff Blair <gmblair@gmail.com>
2015-08-18 13:21:48 -0700

    Clean up build scripts

--- Makefile
+++ Makefile
@@ -1,18 +1,8 @@
+all: clean
+	./build.sh
+
 clean:
 	rm -rf build
-
-build: clean
-	mkdir build
-	browserify main.js -o build/build.js
-	uglifyjs build/build.js \
-		-o build/build.js --overwrite \
-		--compress unsafe,screw_ie8,hoist_vars \
-		--mangle toplevel \
-		--mangle-props
-	cp index.html build/index.html
-	cp -R media build
-	# TODO: The $line variable is coming out blank
-	#find build -name '*.png' | while read line; do pngcrush -ow -brute "$line"; done
 
 develop:
 	watchify main.js -o build.js --debug
---
+++ build.sh
@@ -0,0 +1,23 @@
+# Build/minify/mangle JavaScript
+browserify main.js -o build.js
+uglifyjs build.js \
+	-o build.js --overwrite \
+	--compress unsafe,screw_ie8,hoist_vars \
+	--mangle toplevel \
+	--mangle-props
+
+# Copy files to build folder
+mkdir -p build
+cp -R index.html build.js media build
+cd build
+
+# Compress images using pngcrush
+# TODO: The $line variable is coming out blank
+#find build -name '*.png' | while read line; do pngcrush -ow -brute "$line"; done
+
+# Compress files into final zip package
+# Options: quiet, recursive, best compression
+zip -qr9 package.zip index.html build.js media
+
+# Output the package size
+ls -l package.zip
